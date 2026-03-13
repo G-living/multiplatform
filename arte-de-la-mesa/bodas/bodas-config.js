47b89e4adf0baefc5d6cb1dfaa630ca1ad3bc4f4
G-living <107722892+G-living@users.noreply.github.com>
2026-03-13 07:43:14 -0500

    Update bodas-config.js

--- arte-de-la-mesa/bodas/bodas-config.js
+++ arte-de-la-mesa/bodas/bodas-config.js
@@ -3,7 +3,7 @@
  * Cargado antes del script inline en bodas-landing.html y bodas-guest.html
  */
 const BODAS_CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzuvc8d_EpzzuiiWX9FRudG9X7EtuZ5z2-6oxmKOjehOdKyzkj5zD9DnrtEwk6EEY1W/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzEcnqlH80fJbVLEk34dzFc-Tu5IpBzTeVB0j4flb6tuFHifM6PAK_l4fnocEGR5-Ho/exec',
   API_TOKEN: 'bodas-GLv2-XkP9mTqR7hNwJ3sE',
   WOMPI_PUB_KEY: 'pub_prod_PENDIENTE',           // reemplazar con clave pública Wompi producción
   SIGN_WORKER: 'https://mlg-signature-generator.filippo-massara2016.workers.dev/bodas',
